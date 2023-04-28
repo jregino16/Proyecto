b7a31b6417141b9a9e26f5e9cf97577903a66270
jhon sebastian <jhonregino16@hotmail.com>
2023-04-27 20:54:04 -0600

    Modelo con las migraciones

--- database/migrations/2023_04_28_020430_create_personas_table.php
+++ database/migrations/2023_04_28_020430_create_personas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('personas', function (Blueprint $table) {
             $table->id();
+            $table->string('documento');
+            $table->string('nombre');
+            $table->string('direccionResidencia');
+            $table->string('estrato');
             $table->timestamps();
         });
     }
